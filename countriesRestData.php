59c684aaeb70fe502d798539c7a061c8dfdc95fa
sultanshaik <sultanshaik.dev@gmail.com>
2018-03-07 02:22:13 -0500

    Add code for sorting and limiting

--- countriesRestData.php
+++ countriesRestData.php
@@ -1,11 +1,14 @@
 <?php
-$countryName = "USA";
+$countryName = "U";
   if (!empty($countryName)) { 
 	$countryDataUrl = 'https://restcountries.eu/rest/v2/name/'.urlencode($countryName);
 	$countryDataJSON =  file_get_contents($countryDataUrl);	
 	$countryDataPHPArray = json_decode($countryDataJSON,true);
-	echo $countryDataPHPArray[0]["name"];
-	
-	
-}
+	array_multisort($countryDataPHPArray);
+	$countryDataArrayLimited = array_slice($countryDataPHPArray, 0, 50);
+	foreach ($countryDataArrayLimited as $eachCountryData) {
+    echo $eachCountryData["name"];
+	echo "<br>";
+	}
+  }	
 ?>
