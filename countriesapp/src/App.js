c623c79c6d699952b3fbffa2aeb92fb22287553c
sultanshaik <sultanshaik.dev@gmail.com>
2018-03-07 03:36:22 -0500

    Add form and buttons for submit and reset

--- countriesapp/src/App.js
+++ countriesapp/src/App.js
@@ -5,14 +5,14 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="">
+	  <form>
+	<p>Enter the country name or related search string</p>
+	<input 	type="text"
+	placeholder="Country name" />
+<button className="">Submit</button>
+<button className="">Reset</button>
+</form>
       </div>
     );
   }
