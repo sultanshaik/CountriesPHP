4e1f91008f649a85965bbe1bfd4b8d3f8f8cf38e
sultanshaik <sultanshaik.dev@gmail.com>
2018-03-09 05:33:56 -0500

    Index.php for output rendering

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include './countriesRestData.php';?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<link rel="shortcut icon" href="images/globe.png" type="image/xicon">
+</head>
+<body>
+</body>
+</html>
