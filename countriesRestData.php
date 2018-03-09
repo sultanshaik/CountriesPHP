4ab1f069d62e4a2735014b5e6db234e8543b3ceb
sultanshaik <sultanshaik.dev@gmail.com>
2018-03-09 00:33:49 -0500

    Add code for sending url parameter

--- countriesRestData.php
+++ countriesRestData.php
@@ -1,14 +1,12 @@
 <?php
-$countryName = "U";
+$countryName = $_GET['countrystring'];
   if (!empty($countryName)) { 
+	
 	$countryDataUrl = 'https://restcountries.eu/rest/v2/name/'.urlencode($countryName);
 	$countryDataJSON =  file_get_contents($countryDataUrl);	
 	$countryDataPHPArray = json_decode($countryDataJSON,true);
 	array_multisort($countryDataPHPArray);
 	$countryDataArrayLimited = array_slice($countryDataPHPArray, 0, 50);
-	foreach ($countryDataArrayLimited as $eachCountryData) {
-    echo $eachCountryData["name"];
-	echo "<br>";
-	}
-  }	
+	echo json_encode($countryDataArrayLimited,true);
+  }
 ?>
