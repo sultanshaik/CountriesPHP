e830cc906ffe37397abce1f0a62b08c5b61febef
sultanshaik <sultanshaik.dev@gmail.com>
2018-03-11 09:51:52 -0400

    Add code for countIncrease and component update

--- countriesapp/src/Countrycomponent.js
+++ countriesapp/src/Countrycomponent.js
@@ -1,28 +1,58 @@
 import React, { Component } from 'react';
+import './Countrycomponent.css';
 
 class Countrycomponent extends Component {
 	constructor(props) {
     super(props);
-    this.state = {countrystring: this.props.countrystring,countries:[]};
+    this.state = {countries:[]};
     
 	}
+	filter(countries){
+		return	countries;
+	}
 	componentDidMount(){
-		console.log("Enter");
-				fetch("http://localhost:8080/?countrystring="+ this.state.countrystring,{
-			method:'get',
-			
+				fetch("http://localhost:8080/?countrystring="+ this.props.countrystring,{
+			method:'get',	
 		}).
 		then(response=>response.json()).
-		then(responseData=>{console.log(responseData)})
-	console.log("Exit");
+		then(countries=>{
+			this.setState({countries:this.filter(countries)});
+			this.countIncrease();
+		})
 
 	}
+	
+	countIncrease(){
+		this.props.countIncrease();
+	}
+
+	componentWillReceiveProps(nextProps){
+		if(nextProps.countrystring!==this.props.countrystring){
+				fetch("http://localhost:8080/?countrystring="+ nextProps.countrystring,{
+			method:'get',	
+		}).
+		then(response=>response.json()).
+		then(countries=>{
+			this.setState({countries:this.filter(countries) });
+			this.countIncrease();
+		})
+		}
+	}
+	
   render() {
     return (
       <div className="">
 		
-		<p>hello</p>
-		{this.state.countries.name}
+		{this.state.countries.map(x=>{
+			return(
+				<div>
+					{x.name},{x.alpha2Code}
+					<img className="Flageimage" src = {x.flag}/>
+				</div>
+				)
+			}
+		)
+		}
       </div>
     );
   }
